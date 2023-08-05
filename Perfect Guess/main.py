7699f12ffbf62c3440f717d7ed6199e07dbbaa61
Vedrockerz <vedtherockerz@gmail.com>
2023-08-05 10:47:37 -0700

    Perfect guess modified

--- Perfect Guess/main.py
+++ Perfect Guess/main.py
@@ -7,7 +7,6 @@ def myGame():
     userGuess = None
     guesses = 0
     while(userGuess!=randNo):
-        try:
             userGuess = int(input("Enter your guess between 1 to 100 : "))
             guesses += 1 
 
@@ -15,21 +14,18 @@ def myGame():
                 print("You Guessed it right!")
             else:
                 if(userGuess>randNo):
-                    print("You guessed it wrong! enter a smaller no.")
+                    print("You guessed it wrong! a smaller no.")
                 else:
                     print("You guessed it wrong! enter a larger no.")
-        except Exception as e:
-            print("Please enter a valid value")
-            # print(e)
 
     print(f"You guessed it in {guesses} times")
 
-    with open("D:/vs/python/hiscr.txt" , "r") as f:
+    with open("Perfect Guess\hiscore_PG.txt" , "r") as f:
         hs = int(f.read())
         
     if (guesses<hs):
         print(f" NEW HIGH SCORE! = {guesses}")
-        with open("D:/vs/python/hiscr.txt" , "w") as f:
+        with open("Perfect Guess\hiscore_PG.txt" , "w") as f:
             f.write(str(guesses))
     else:
         print(f"HIGH SCORE is {hs}")
---
+++ Perfect Guess/multiplayer.py
@@ -0,0 +1,40 @@
+# Playing by multiplyers:
+import random
+
+try:
+    def guess_Game(a,b,actual):
+        trials = 1
+        user_Guess = int(input(f"Enter the guess number between {a} and {b}\n"))
+        while actual != user_Guess :
+            if actual > user_Guess:
+                user_Guess = int(input(f"Enter the larger number\n"))
+                trials+=1
+            else : 
+                user_Guess = int(input(f"Enter the smaller number\n"))
+                trials+=1
+
+        print(f"Congrats! You guessed it in {trials}")
+        return trials
+
+    if __name__ == '__main__' :
+        a = int(input("Enter the value of a\n"))
+        b = int(input("Enter the value of b\n"))
+
+        actual1 = random.randint(a,b)
+        print("***** Player 1's Turn *****\n")
+        p1 = guess_Game(a,b,actual1)
+        
+        actual2 = random.randint(a,b)
+        print("***** Player 2's Turn *****\n")
+        p2 = guess_Game(a,b,actual2)
+
+        if p1<p2 : 
+            print("Player 1 wins..!")
+        elif p2<p1 : 
+            print("Player 2 wins..!")
+        else : 
+            print("Match Tie..!")
+
+        print(f"Player 1's number is {actual1} and Player 2's number is {actual2} ")
+except Exception as e:
+    print("------  ERROR  --------")
